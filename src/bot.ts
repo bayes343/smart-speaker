32c79d90b6e41b71bbdf89acaec80a33c07db9bf
Joey Bayes <joey.bayes@uscca.com>
2022-08-25 00:06:18 -0400

    Display the default bot name.  Exclude non-local voices from list.

--- src/bot.ts
+++ src/bot.ts
@@ -2,38 +2,43 @@ import { AsyncObservable } from 'tsbase/Patterns/Observable/AsyncObservable';
 import { ISpeechCommand } from 'tsbase/Utility/Speech/ISpeechCommand';
 import { SpeechRecognizer, SpeechSynthesizer } from 'tsbase/Utility/Speech/module';
 
-speechSynthesis.getVoices(); // bug with web api? - doesn't find voices on line 9 without this
-
 export class Bot {
   private static instance: Bot | null = null;
   public static Instance = (
-    sr = new SpeechRecognizer(),
-    ss = new SpeechSynthesizer()
-  ): Bot => this.instance = this.instance || new Bot(sr, ss);
+    activeSpeechRecognizer = new SpeechRecognizer(),
+    // passiveSpeechRecognizer = new SpeechRecognizer(),
+    speechSynthesizer = new SpeechSynthesizer()
+  ): Bot => this.instance = this.instance || new Bot(activeSpeechRecognizer, speechSynthesizer);
   public static Destroy = (): void => { this.instance = null; };
 
   public Speaker = new AsyncObservable<string>();
   public Commands: ISpeechCommand[] = [];
-  private get voice() {
-    return speechSynthesis.getVoices().filter(v => v.lang === 'en-US')[2];
-  }
   private stopListening = false;
 
+  private voice: SpeechSynthesisVoice | null = null;
+  public get Voice(): SpeechSynthesisVoice {
+    return this.voice = this.voice || speechSynthesis.getVoices().filter(v => v.localService && v.lang.startsWith('en'))[1];
+  }
+  public set Voice(v: SpeechSynthesisVoice) {
+    this.voice = v;
+  }
+
   private constructor(
-    private sr: SpeechRecognizer,
-    private ss: SpeechSynthesizer
+    private activeSpeechRecognizer: SpeechRecognizer,
+    // private passiveSpeechRecognizer: SpeechRecognizer,
+    private speechSynthesizer: SpeechSynthesizer
   ) {
     setTimeout(() => { // push this sub to below the one in welcome
       this.Speaker.Subscribe(async (transcript) => {
         if (transcript) {
-          await this.ss.Speak(transcript, this.voice);
+          await this.speechSynthesizer.Speak(transcript, this.Voice);
         }
       });
     });
   }
 
   public async Activate(): Promise<void> {
-    await this.sr.HandleSpeechCommands(this.Commands, () => this.stopListening);
+    await this.activeSpeechRecognizer.HandleSpeechCommands(this.Commands, () => this.stopListening);
     this.stopListening = false;
   }
 
